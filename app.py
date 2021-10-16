78b25cd17430ac0419b2e11c0a585e4b6f82890c
Arina Apetyan <aaapetyan@gmail.com>
2021-10-17 00:24:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api, abort, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
@@ -55,35 +55,50 @@ search_parser.add_argument('last_name', required=True)
 search_parser.add_argument('first_name', required=True)
 search_parser.add_argument('father_name', required=True)
 
-input_full = {'last_name': str, 'first_name': str, 'father_name': str, 'birth_year': int,
-              'id': int, 'salary': float, 'position': str, 'legal_name': str, 'department': str}
-input_search = {'last_name': str, 'first_name': str, 'father_name': str}
+# correct input data types for a new record
+input_types = {'last_name': str, 'first_name': str, 'father_name': str, 'birth_year': int,
+               'id': int, 'salary': float, 'position': str, 'legal_name': str, 'department': str}
 
 
+# decorator for validation of input data
 def validate(types):
     def decorator(method):
         @wraps(method)
         def wrapper(self):
             args = parser.parse_args()
+            # checking if the arguments collected from the request are of the correct type
             for param in args:
-                if type(args[param]) is types[param]:
-                    pass
-                else:
+                if type(args[param]) is not types[param]:
                     return f'Error! {param} should be {types[param]} not {type(args[param])}'
             return method(self)
         return wrapper
     return decorator
 
 
+# decorator for getting a result or the error
+def result_or_error(method):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        try:
+            response = method(self, *args, **kwargs)
+            return {'result': response}
+        except Exception as e:
+            return {'error': str(e)}
+    return wrapper
+
+
 class EmployeesRecordList(Resource):
     # to get the full list of employees
+    @result_or_error
     def get(self):
         records = EmployeesRecord.query.all()
         return [EmployeesRecord.serialize(record) for record in records]
 
     # to add a new employee to the list
-    @validate(input_full)
+    @result_or_error
+    @validate(input_types)
     def post(self):
+        # collecting data from the request
         args = parser.parse_args()
         employee_record = EmployeesRecord(last_name=args['last_name'],
                                           first_name=args['first_name'],
@@ -94,21 +109,29 @@ class EmployeesRecordList(Resource):
                                           position=args['position'],
                                           legal_name=args['legal_name'],
                                           department=args['department'])
+        requested_id = args['id']
+        # checking if the record with this id already exists
+        if EmployeesRecord.query.filter_by(id=requested_id).first() is not None:
+            abort(f'Record with id={requested_id} already exists')
         db.session.add(employee_record)
         db.session.commit()
-        return EmployeesRecord.serialize(employee_record), 201
+        return EmployeesRecord.serialize(employee_record)
 
 
 class Employee(Resource):
     # to search for the employee by id
+    @result_or_error
     def get(self, emp_id):
         return EmployeesRecord.serialize(
             EmployeesRecord.query.filter_by(id=emp_id)
             .first_or_404(description=f'Record with id={emp_id} is not available'))
 
     # to update the information for the existing employee
+    @result_or_error
     def put(self, emp_id):
+        # collecting data from the request
         args = parser.parse_args()
+        # finding the record we are going to modify
         record = EmployeesRecord.query.filter_by(id=emp_id)\
             .first_or_404(description=f'Record with id={emp_id} is not available')
         record.last_name = args['last_name']
@@ -121,30 +144,33 @@ class Employee(Resource):
         record.legal_name = args['legal_name']
         record.department = args['department']
         db.session.commit()
-        return EmployeesRecord.serialize(record), 201
+        return EmployeesRecord.serialize(record)
 
     # to delete an employee from the list
+    @result_or_error
     def delete(self, emp_id):
         record = EmployeesRecord.query.filter_by(id=emp_id)\
             .first_or_404(description=f'Record with id={emp_id} is not available')
         db.session.delete(record)
         db.session.commit()
-        return '', 204
+        return 'The record has been deleted'
 
 
 class SearchEmployee(Resource):
     # to search for an employee by name
+    @result_or_error
     def get(self):
-        args = search_parser.parse_args()
-        records = EmployeesRecord.query.filter_by(last_name=args['last_name'],
-                                                  first_name=args['first_name'],
-                                                  father_name=args['father_name'])
+        # collecting search data from the request
+        search_args = search_parser.parse_args()
+        records = EmployeesRecord.query.filter_by(last_name=search_args['last_name'],
+                                                  first_name=search_args['first_name'],
+                                                  father_name=search_args['father_name'])
         return [EmployeesRecord.serialize(record) for record in records]
 
 
-api.add_resource(EmployeesRecordList, '/recordsAll', '/')
-api.add_resource(Employee, '/record/<int:emp_id>')
-api.add_resource(SearchEmployee, '/search')
+api.add_resource(EmployeesRecordList, '/recordsAll', '/', methods=['GET', 'POST'])
+api.add_resource(Employee, '/record/<int:emp_id>', methods=['GET', 'PUT', 'DELETE'])
+api.add_resource(SearchEmployee, '/search', methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
