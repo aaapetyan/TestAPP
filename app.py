45b8627ec5f5ca010ea8ad14ce1728ce9ffdb415
Arina Apetyan <aaapetyan@gmail.com>
2021-10-17 21:56:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api, abort, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
